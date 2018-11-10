519819ddfe11b164ef001f9f6da7200a4822b2df
Mikhail Pontus <m.pontus@gmail.com>
2018-11-10 14:03:31 +0300

    Use separate database for e2e tests

--- server/test/utils/initApp.ts
+++ server/test/utils/initApp.ts
@@ -3,6 +3,10 @@ import { useContainer } from 'class-validator';
 import { AppModule } from '../../src/app.module';
 
 export const initApp = async () => {
+  // Avoid breaking dev environement during tests using separate db
+  process.env.DATABASE_URL = process.env.TEST_DATABASE_URL;
+  process.env.REDIS_URL = process.env.TEST_REDIS_URL;
+
   const nestApp = await NestFactory.create(AppModule, {
     logger: false,
   });
